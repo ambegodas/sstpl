3e913fda664c0727ce599cd0fea855d0ef51a682
pathmasri <ambegodas@gmail.com>
2015-10-24 11:55:40 +0400

    Initial Commit

---
+++ src/main/java/lk/sharpersharp/controller/MainController.java
@@ -0,0 +1,12 @@
+package lk.sharpersharp.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by pathmasri on 10/23/15.
+ */
+
+@Controller
+public class MainController {
+
+}
